1a9639c6724dfaf0615a447c38e4a536bb5f26c9
selbip <dltmfql86323@gmail.com>
2026-09-02 15:37:02 +0900

    20260902_환자상세

--- backend/apps/patients/urls.py
+++ backend/apps/patients/urls.py
@@ -1,9 +1,10 @@
 # 원무과
 from django.urls import path
 
-from .views import PatientListAPIView
+from .views import PatientListAPIView, PatientDetailAPIView
 
 
 urlpatterns = [
     path("", PatientListAPIView.as_view(), name="patient-list"),
+    path("<uuid:id>/", PatientDetailAPIView.as_view(), name="patient-detail"),
 ]
--- backend/apps/patients/views.py
+++ backend/apps/patients/views.py
@@ -1,10 +1,16 @@
 # 원무과
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView
 
 from .models import Patient
 from .serializers import PatientSerializer
 
 
-class PatientListAPIView(ListAPIView):
+class PatientListAPIView(ListAPIView): # 환자 전체 목록 조회
     queryset = Patient.objects.all().order_by("-created_at")
     serializer_class = PatientSerializer
+
+
+class PatientDetailAPIView(RetrieveAPIView): # PatientDetailAPIView
+    queryset = Patient.objects.all()
+    serializer_class = PatientSerializer
+    lookup_field = "id"
